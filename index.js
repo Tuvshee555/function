e794dd0f37751ac473d7a7b1784eddadca3e5f47
Tushka555 <ganturtuvshinsihan@gmail.com>
2024-12-10 23:01:39 +0800

    chnge

--- index.js
+++ index.js
@@ -204,69 +204,105 @@
 //       return "mixed";
 //     }
 //   }
-//   console.log(getCase("javascript..")); 
-//   console.log(getCase("SHOUT!"));    
-//   console.log(getCase("JaVaScRiPt")); 
-//   console.log(getCase("12345"));      
+//   console.log(getCase("javascript.."));
+//   console.log(getCase("SHOUT!"));
+//   console.log(getCase("JaVaScRiPt"));
+//   console.log(getCase("12345"));
 
 // Exercise 11
- 
+
 // Create a function that takes  a string as argument (of a person’s first and last name) and returns a string with the first and last name swapped.
 // swapName("Ada Lovelace") ➞ "Lovelace Ada"
 // swapName(“Alan Turing”)➞  “Turing Alan”
- 
+// function swapName(name) {
+//     let [first, last] = name.split(" ");
+//     return `${last} ${first}`;
+// }
+
+// console.log(swapName("Ada Lovelace"));
+// console.log(swapName("Alan Turing"));
+
 // Exercise 12
- 
+
 // Create a function that takes a string and returns a string with its letters in alphabetical order.
 // AlphabetSoup("javascript") ➞ "aacijprstv"
 // AlphabetSoup(“simplon”) ➞”ilmnops”
- 
+// function AlphabetSoup(str) {
+//     return str.split('').sort().join('');
+// }
+
+// console.log(AlphabetSoup("javascript"));
+// console.log(AlphabetSoup("simplon"));
+
 // Exercise 13
- 
+
 // Create a function that takes a number as argument and returns the incrementing (+1) for each odd number and decrementing (-1) for each even number.
 // incrementOrDecrement(5) ➞4
 // incrementOrDecrement(2) ➞3
- 
-// console.log("functions2");
- 
+// function incrementOrDecrement(num5){
+//     if (num5 % 2 === 0){
+//         return num5+1
+//     } else  if (num5 % 2 !== 0){
+//         return num5-1
+//     }
+// }
+//  console.log(incrementOrDecrement(5))
+//  console.log(incrementOrDecrement(2))
 // Exercise 1
- 
+
 // Write a function "tipAmount" that is given the bill amount and the level of
 // service (one of good, fair and poor) and returns the dollar amount for the tip.
- 
+
 // Based on:
 // good --> 20%
 // fair --> 15%
 // poor --> 10%
- 
+
 // Examples:
 // tipAmount(100, 'good') --> 20
 // tipAmount(40, 'fair') --> 6
- 
+function tipAmount(bill, service) {
+  let tipPercentage;
+  if (service === "good") {
+    tipPercentage = 0.2;
+  } else if (service === "fair") {
+    tipPercentage = 0.15;
+  } else if (service === "poor") {
+    tipPercentage = 0.1;
+  } else {
+    return "Invalid service level";
+  }
+  return bill * tipPercentage;
+}
+
+// Example usage:
+console.log(tipAmount(100, "good"));
+console.log(tipAmount(40, "fair"));
+
 // Exercise 2
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "totalAmount" that takes the same arguments as "tipAmount"
 // except it returns the total as the tip amount plus the bill amount.
 // Hint: this function may use "tipAmount" internally
- 
+
 // Examples:
 // totalAmount(100, 'good') --> 120
 // totalAmount(40, 'fair') --> 46
- 
+
 // Exercise 3
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "splitAmount" that takes a bill amount, the level of service,
 // and the number of people to split the bill between. It should return the final
 // amount for each person.
- 
+
 // Examples:
 // splitAmount(100, 'good', 5) --> 24
 // splitAmount(40, 'fair', 2) --> 23
- 
+
 // Exercise 4
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "isVowel" that takes a character (i.e. a string of length 1)
 // as input and returns true if it is a vowel, false otherwise.
@@ -280,9 +316,9 @@
 // isVowel('A') --> true
 // isVowel(99) --> false
 // isVowel({e: 'Elephant'}) --> false
- 
+
 // Exercise 5
- 
+
 // Write a function "rockPaperScissors" which takes the throw of player 1 and
 // the throw of player 2.
 // A throw can have the values of 'rock', 'paper', or 'scissors'.
@@ -291,20 +327,20 @@
 // rockPaperScissors('rock', 'scissors') --> 'player 1'
 // rockPaperScissors('rock', 'paper') --> 'player 2'
 // rockPaperScissors('paper', 'paper') --> 'draw'
- 
+
 // Exercise 6
- 
+
 // Write a function "numberJoinerWhile" which is given a start number and an end number.
 // It should return a string of the numbers joined together by the "_" character.
 // Use a "while" loop to do this.
 // Examples:
 // numberJoinerWhile(1, 10) --> '1_2_3_4_5_6_7_8_9_10'
 // numberJoinerWhile(12, 14) --> '12_13_14'
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "numberJoinerFor" which does the same thing as "numberJoinerWhile",
 // except using a "for" loop internally.
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "numberJoinerFancy" which does the same thing as "numberJoinerWhile",
 // except it takes an optional third argument specifying the separator between the numbers.
@@ -313,15 +349,15 @@
 // numberJoinerFancy(1, 10) --> '1_2_3_4_5_6_7_8_9_10'
 // numberJoinerFancy(1, 5, '~') --> 1~2~3~4~5
 // numberJoinerFancy(3, 6, '***BANANAS***') --> 1***BANANAS***2***BANANAS***3
- 
+
 // Exercise 7
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "reverse" that computes the reversal of a string.
 //
 // Example:
 // reverse("skoob") --> "books"
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "findLongestWord" that takes a string of words and returns
 // the longest word in that string. If there are multiple words with the same
@@ -329,7 +365,7 @@
 //
 // Example:
 // findLongestWord('a book full of dogs') --> 'book'
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function called "nicer"
 // It should clean up the language in its input sentence.
@@ -338,9 +374,9 @@
 // Example:
 // nicer('mom get the heck in here and bring me a darn sandwich.')
 // > 'mom get the in here and bring me a sandwich.'
- 
+
 // Exercise 8
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function called "capitalizeAll"
 // It should take as input a sentence and capitalize the first letter
@@ -349,7 +385,7 @@
 // Examples:
 // capitalizeAll('hello world') --> 'Hello World'
 // capitalizeAll('every day is like sunday') --> 'Every Day Is Like Sunday'
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function called "split" that does the same thing as String.split
 // It should take two inputs: (1) a string and (2) a delimiter string
@@ -360,18 +396,18 @@
 // split('a-b-c', '-') --> ['a', 'b', 'c']
 // split('APPLExxBANANAxxCHERRY', 'xx') --> ['APPLE', 'BANANA', 'CHERRY']
 // split('xyz', 'r') --> ['xyz']
- 
+
 // Exercise 9
- 
+
 // Write a function "max" that takes an array of numbers returns the highest
 // number in the array.
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "sumNumbers" which is given an array of numbers and returns
 // the sum of the numbers.
 // Example:
 // sumNumbers([1, 4, 8]) --> 13
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "positives" which is given an array of numbers and
 // returns a new array containing only the positive numbers within the given array.
@@ -379,90 +415,89 @@
 // positives([1, -3, 5, -3, 0]) --> [1, 5]
 // positives([1, 2, 3]) --> [1, 2, 3]
 // positives([-1, -2, -3]) --> []
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "evens" which takes an array of numbers and returns a new
 // array containing only the even numbers in the given array.
 // Hint: you may want to re-use your "isEven" function from 01-predicate-functions.js
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "odds" which takes an array of numbers and returns a new
 // array containing only the odd numbers in the given array.
 // Hint: you may want to re-use your "isOdd" function from 01-predicate-functions.js
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "integers" which takes an array of numbers and returns a new
 // array containing only the integers in the given array.
 // Hint: Do you need a new predicate function for this?
- 
+
 //
 // Example:
 // integers([3.14, 2.4, 7, 8.1, 2]) --> [7, 2]
- 
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "squareDance" which takes an array of numbers and returns a
 // new array containing the result of squaring each of the numbers in the given array.
 //
 // Example:
 // squareDance([1, 2, 3]) --> [1, 4, 9]
- 
+
 //Exercise
- 
+
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
- 
+
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
- 
+
 // You can return the answer in any order.
- 
+
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
- 
+
 //Exercise
- 
+
 // Given an integer x, return true if x is palindrome integer.
- 
+
 // An integer is a palindrome when it reads the same backward as forward.
- 
+
 // For example, 121 is a palindrome while 123 is not.
- 
+
 // Input: x = 121
 // Output: true
 // Explanation: 121 reads as 121 from left to right and from right to left.
- 
+
 //Exercise
- 
+
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
- 
+
 // An input string is valid if:
- 
+
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
- 
+
 // Example 1:
- 
+
 // Input: s = "()"
 // Output: true
 // Example 2:
- 
+
 // Input: s = "()[]{}"
 // Output: true
- 
+
 //Exercise
- 
+
 // write a JavaScript function that checks if the string is palindrome or not.
- 
+
 // A string is a palindrome if it is read the same from forward or backward.
 //  For example, dad reads the same either from forward or backward. So the word dad is a palindrome.
 // Similarly, madam is also a palindrome.
- 
+
 // Input: s = "dad"
 // Output: true
 // Example 2:
- 
+
 // Input: s = "civic"
 // Output: true
- 
+
 // Input: s = "race"
 // Output: false
- 
