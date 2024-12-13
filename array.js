7b4769b3d0a5eb238f1023492e37ad36432b3904
24LP2767 24LP2767 <24LP2767@G301-CC0194.local>
2024-12-13 18:01:56 +0800

    map

--- array.js
+++ array.js
@@ -222,91 +222,216 @@ i.e. is31Flavors(originalFlavors) will return TRUE.*/
 // function addFlavor(add){
 //     for (i=0; )
 // }
-// numbers.unshift(1234123);
-// console.log(`Unshifted array ${numbers}`);
+
+// function addFlavor(arr, flavor) {
+//   arr.unshift(flavor);
+//   console.log(arr);
+
+// }
+// console.log(addFlavor(originalFlavors, "Rainbow Sherbert"))
 
 //
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array.
- 
 Your function should accept:
- 
 (1) an array
- 
 Your function should remove a flavor from the end of the array and console.log the resulting array.
- 
- 
- 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/
+// function addFlavor(arr, flavor) {
+//   arr.unshift(flavor);
+//   arr.pop()
+//   console.log(arr);
+
+// }
+// console.log(addFlavor(originalFlavors, "Rainbow Sherbert"))
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
- 
 Your function should accept:
- 
 (1) an array
 (2) an index
- 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-/* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array.
- 
-Your function should accept:
- 
-(1) an array
-(2) a string (flavor)
- 
-For example, removeFlavorByName(originalFlavors, "Vanilla") would return an array with the length 30 including all of the flavors except Vanilla.
- 
-Hint: You can use .splice() for this
- 
-*/
+// function getFlavorByIndex(arr, index) {
+//   return arr[index];
+// }
+// console.log(getFlavorByIndex(originalFlavors, 2));
 
-/* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array.
- 
-Your function should accept:
- 
-2 arguments 1 for your new array and one for your original array
- 
-and should return a new array that is identical to the old array. You can name the new array however you'd like. */
+//  Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array.
+// Your function should accept:
+// (1) an array
+// (2) a string (flavor)
+// For example, removeFlavorByName(originalFlavors, "Vanilla") would return an array with the length 30 including all of the flavors except Vanilla.
+// Hint: You can use .splice() for this
 
-/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
- 
-Your function should accept:
- 
-(1) an Array
-(2) a string (i.e. "chocolate")
- 
-and return a new array.
- 
-For example, filterByWord(originalFlavors, "Chocolate") should return ["Chocolate", "Chocolate Almond",..."Chocolate Ribbon"].
- 
-DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
- 
-hint - you can use the .includes method to help you solve this */
-
-// function filterByWord(array, word) {
-//   const filteredArray = [];
-//   array.forEach(function (flavor) {
-//     if (flavor.includes(word)) {
-//       console.log(flavor);
+// function removeFlavorByName(arr, flavor) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === flavor) {
+//       arr.splice(i, 1);
 //     }
-//   });
-//   //   return filteredArray;
+//   }
+
+//   return arr;
 // }
 
+// console.log(removeFlavorByName(originalFlavors, "Vanilla"));
+
+/* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array.
+Your function should accept:
+2 arguments 1 for your new array and one for your original array
+and should return a new array that is identical to the old array. You can name the new array however you'd like. */
+// function copyArray(originalArr) {
+//   let copy = Object.assign([], originalArr);
+//   return copy;
+// }
+// console.log(copyArray(originalFlavors));
+
+//  Task 7: July 7th is "World Chocolate Day" and Alpen gold wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every
+// item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string
+// as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
+// Your function should accept:
+// (1) an Array
+// (2) a string (i.e. "chocolate")
+// and return a new array.
+// For example, filterByWord(originalFlavors, "Chocolate") should return ["Chocolate", "Chocolate Almond",..."Chocolate Ribbon"].
+// DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
+// hint - you can use the .includes method to help you solve this */
+// function filterByWord(array, word) {
+//   const filteredArray = [];
+
+//   array.forEach(function (flavor) {
+//     if (flavor.toLowerCase().includes(word.toLowerCase())) {
+//       filteredArray.push(flavor);
+//     }
+//   });
+
+//   return filteredArray;
+// }
 // console.log(filterByWord(originalFlavors, "chocolate"));
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/
 
-/* STRETCH 1: Write a function that returns the average number of words in an array. You should be able to use this function for any array, but can test with originalFlavors.
- 
-Your function should accept:
- 
-(1) an array
- 
-and should return the average number of words per item in the array.
- 
-For example, getAverageWordLength(originalFlavors) should return a number between 0 and 3. */
+// function includeChocolate(arr, choco) {
+//   let chos = [];
 
-// function getAverageWordLength(/*code here*/) {
-/*code here*/
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].toLowerCase().includes(choco.toLowerCase())) {
+//       chos.push(arr[i]);
+//     }
+//   }
+
+//   return chos;
 // }
+// console.log(includeChocolate(originalFlavors, "chocolate"));
+
+//  STRETCH 1: Write a function that returns the average number of words in an array. You should be able to use this function for any array, but can test with originalFlavors.
+// Your function should accept:
+// (1) an array
+// and should return the average number of words per item in the array.
+// For example, getAverageWordLength(originalFlavors) should return a number between 0 and 3. */
+// function getAverageWordLength(array) {
+//   let num = 0;
+//   array.forEach(function (flavor) {
+//     num += flavor.split(" ").length;
+//   });
+//   return num / array.length
+// }
+// console.log(getAverageWordLength(originalFlavors));
+
+//PART 2 ---------------------------------------------------------
+
+// Array exercises 2
+// Exercise 1
+// Write a JavaScript function to sort three numbers in array. Return sorted array.
+// Sample numbers : [0, -1, 4]
+// Output : [4, 0, -1]
+// notes:
+//     https://www.geeksforgeeks.org/javascript-array-sort/
+//     https://www.w3schools.com/jsref/met_win_alert.asp
+
+// 1. write function ==> function will take array as input
+// 2. sort the numbers inside the array and return sorted array
+
+// function sortedArray(array) {
+//   array.sort((a, b) => b - a);
+//   return array;
+// }
+
+// console.log(sortedArray([5, 7, 10, -10, 56])); // ==> [56, 10, 7, 5, -10]
+
+// Exercise 1
+// Create a function that will display the smallest value in the array.
+// Example:
+// > console.log(findSmallest([30, 45, 60, 7]));
+// > 1
+
+function findSmallest(arr) {
+  let smallest = arr[0];  // Start by assuming the first element is the smallest
+  
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];  // If current value is smaller, update smallest
+    }
+  }
+  
+  return smallest;  // Return the smallest value
+}
+
+console.log(findSmallest([30, 45, 60, 7]));  // Output: 7
+
+
+// Exercise 2
+
+// Create a function that will accept an array, check the data type of each element. The function will delete string elements and will return a the new array
+
+// Example:
+// > console.log(numbersOnly(['text', 3, 7, 'github', 13, 'dev']));
+// > [ 3, 7, 13 ]
+
+// Exercise 3
+
+// Create a function that will accept an array and do the following:
+// Get the lowest element
+// Get the highest element
+// Get the length of array
+// Get the Average of all element;
+// Store these criteria in a new array
+
+// Example:
+// > console.log(minMaxLengthAverage([7, 13, 3, 77, 100]));
+// > [ 3, 100, 5, 40 ]
+
+// Exercise 4
+
+// Count the number of Words
+// Return how many words was given
+
+// Example:
+// > countWords('hello from kbpsystem!');
+// > 3
+
+// Exercise 5
+
+// Multiply by Length
+// Multiply all elements in an array by it's length
+
+// Example:
+// > MultiplyByLength([4,1,1]);
+// > [12, 3, 3]
+
+// Exercise 6
+
+// Find the correct Index location
+// Return the index location of an element from a given array. First argument is the array you'd like to search and the second one is the element (either string/number) to look for.
+
+// Example:
+// > console.log(findIndex(['github', 'gitlab', 'bitbucket', 'apollo'], 'gitlab'));
+// > 1
+
+// MAP FILTER REDUCE
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const result = numbers
+//   .filter(num => num % 2 === 0)
+//   .map(num => num * 2)
+//   .reduce((acc, num) => acc += num,);
+
+// console.log(result);  // 24
